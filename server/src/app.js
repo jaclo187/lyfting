8b35c780721b38ed1a5523d91de5d28e0ed4cb5a
Louis <jaclo187@school.lu>
2020-02-13 11:01:55 +0100

    fixed morgan mode

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const morgan = require("morgan")
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
