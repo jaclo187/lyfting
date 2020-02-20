7b8b4bc0fd17b9d2711b8911285d4a7ac12f488a
Louis <jaclo187@school.lu>
2020-02-20 22:09:32 +0100

    Created config file

---
+++ server/src/config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port : process.env.PORT || 8080,
+    db : {
+        database: process.env.DB_ENV || "lyfting",
+        user: process.env.DB_ENV || "node",
+        password: process.env.DB_PASS || "passw0rd",
+        options: {
+            dialect: process.env.DIALECT || "mysql",
+            host: process.env.HOST || "localhost",
+            storage: "./lyfting.sql"
+        }
+    }
+}
