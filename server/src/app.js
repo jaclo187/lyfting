55111e28a72d26e15c07c613654b208dad0d03f5
Louis <jaclo187@school.lu>
2020-02-20 22:09:10 +0100

    - Added sequelize - Added global config - Moved routes to external file

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,19 @@
 const express = require("express")
 const bodyParser = require('body-parser')
 const cors = require("cors")
+const {sequelize} = require("./models")
 const morgan = require("morgan")
+const config = require('./config/config')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-    res.send({
-        message: `Hello ${req.body.email}!`
-    })
-})
+require('./routes')(app)
 
-app.listen(process.env.port || 8080)
+sequelize.sync()
+    .then(() => {
+        app.listen(config.port)
+        console.log(`Server running on port ${config.port}`)
+    })
