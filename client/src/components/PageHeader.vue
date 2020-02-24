b9ac8a4ec6ed3959462c034a91fdb74072626f18
Louis <jaclo187@school.lu>
2020-02-24 14:35:28 +0100

    Created header

---
+++ client/src/components/PageHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>Lyfting 🏋️‍♂️</v-toolbar-title>
+    <v-toolbar-item>
+      <v-btn flat dark>
+        Home
+      </v-btn>
+    </v-toolbar-item>
+    <v-spacer></v-spacer>
+    <v-toolbar-item>
+      <v-btn flat dark>
+        Sign Up
+      </v-btn>
+    </v-toolbar-item>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  name: 'PageHeader'
+}
+</script>
+<style scoped></style>
