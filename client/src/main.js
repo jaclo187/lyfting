f2ca68fc9383ce25a6eec6df771e56cb29b9ae71
Louis <jaclo187@school.lu>
2020-02-24 14:34:56 +0100

    Added vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,19 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'vuetify/dist/vuetify.min.css'
+import Vuetify from './plugins/vuetify'
+import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
   router,
   store,
+  Vuetify,
+  vuetify,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
