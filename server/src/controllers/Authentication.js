4503cb10c99c7c263c20966e23523eb54c23b983
Louis <jaclo187@school.lu>
2020-02-25 15:21:37 +0100

    Added login and jwt methods

--- server/src/controllers/Authentication.js
+++ server/src/controllers/Authentication.js
@@ -1,17 +1,66 @@
 const {user} = require('../models')
+const ERRORMESSAGE = "Login infromation is invalid"
+const JWTTIME = 60 * 60 * 24 * 7 //ONE WEEK
+const config = require('../config/config')
+const jwt = require('jsonwebtoken')
+
+const jwtSign = user => {
+    return jwt.sign(user, config.jwt.secret, {
+        expiresIn: JWTTIME
+    })
+}
 
 module.exports = {
     async register (req, res) {
-        //console.log(User)
-            //console.log(user)
         try{
             const createdUser = await user.create(req.body)
-            
-            res.send(createdUser.toJSON())
+            const jsonUser = createdUser.toJSON();
+            delete jsonUser.password //Deleting user password form json response
+            res.send({
+                user: jsonUser,
+                token: jwtSign(jsonUser)
+            })
         } catch (e){
             res.status(400).send({
-                error: e
+                error: "Username / Email must be unique"
             })
+        }
+    },
+    async login (req, res) {
+        try{
+            const {email, password} = req.body
+            const loginUser = await user.findOne({
+                where: {
+                    email: email
+                }
+            })
+            if(!loginUser){
+                return res.status(403).send({error: ERRORMESSAGE})
+            }
+            //console.log(loginUser)
+            const isPasswordCorrect = await loginUser.comparePasswords(password)
+            //console.log("Password correct: ", isPasswordCorrect)
+            
+            if(!isPasswordCorrect){
+                return res.status(403).send({error: ERRORMESSAGE})
+            }
+
+            const jsonUser = loginUser.toJSON()
+
+            //console.log(jsonUser)
+
+            delete jsonUser.password //Deleting user password form json response
+
+            res.send({
+                user: jsonUser,
+                token: jwtSign(jsonUser)
+            })
+
+        } catch (e){
+            res.status(500).send({
+                error: ERRORMESSAGE
+            })
+            console.log(e)
         }
     }
 }
