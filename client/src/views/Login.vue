f2ac15e8b93a269ccdf4025933205051dd79c3a9
Louis <jaclo187@school.lu>
2020-02-25 15:19:19 +0100

    Created Login view

---
+++ client/src/views/Login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1 class="pl5">Login</h1>
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="8" sm="4" md="3">
+            <v-text-field v-model="email" label="E-mail"></v-text-field>
+          </v-col>
+          <v-col cols="8" sm="4" md="3">
+            <v-text-field v-model="password" type="password" label="Password"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-btn @click="login">Login</v-btn>
+          </v-col>
+        </v-row>
+        <br />
+      </v-container>
+    </v-form>
+    <v-snackbar v-model="snackbar" timeout="3000" color="error">
+      <span>{{ error }}</span>
+      <v-btn flat color="#ff5252" @click="snackbar = false">Close</v-btn>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: null,
+      snackbar: false
+    }
+  },
+  methods: {
+    async login() {
+      await AuthenticationService.login({
+        email: this.email,
+        password: this.password
+      }).catch(error => {
+        this.error = error.response.data.error
+        this.snackbar = true
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.error {
+  color: red;
+}
+</style>
