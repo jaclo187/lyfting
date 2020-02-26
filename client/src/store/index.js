3397b26507e41f7e5f61374f12cbfd6c8bcf999f
Louis <jaclo187@school.lu>
2020-02-26 11:42:33 +0100

    Login & Logout functions

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,8 +4,37 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    token: null,
+    user: null,
+    isLoggedIn: false
+  },
+  mutations: {
+    setToken(state, token){
+      state.token = token
+      if(token && token !== ""){
+        state.isLoggedIn = true
+      } else state.isLoggedIn = false
+    },
+    setUser(state, user){
+      state.user = user
+    },
+    logoutUser(state){
+      state.user = null
+      state.token = null
+      state.isLoggedIn = false
+    }
+  },
+  actions: {
+    setToken({commit}, token){
+      commit('setToken', token)
+    },
+    setUser({commit}, user){
+      commit('setUser', user)
+    },
+    logoutUser({commit}){
+      commit('logoutUser')
+    }
+  },
   modules: {}
 })
