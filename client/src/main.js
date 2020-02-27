488bb390655c4b35e39560957e2fd33f16ac7ac4
Louis Jacoby <jacoby.louis123@gmail.com>
2020-02-27 18:03:05 +0100

    Fixed Vuetify which was initiated twice

--- client/src/main.js
+++ client/src/main.js
@@ -3,20 +3,18 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'vuetify/dist/vuetify.min.css'
-import Vuetify from './plugins/vuetify'
 import vuetify from './plugins/vuetify'
-import {sync} from 'vuex-router-sync'
+import { sync } from 'vuex-router-sync'
 
 Vue.config.productionTip = false
 
-Vue.use(Vuetify)
+Vue.use(vuetify)
 
 sync(store, router)
 
 new Vue({
   router,
   store,
-  Vuetify,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
