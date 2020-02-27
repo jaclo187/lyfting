2ba67fab9858be4d71476ba9f36b55bcbaff7a3b
Louis Jacoby <jacoby.louis123@gmail.com>
2020-02-27 18:04:29 +0100

    Created componend which Shows a single workout including with all sets

---
+++ client/src/components/WorkoutPanel.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <!-- <div>{{workout}}</div> -->
+    <h4 v-if="workout.name !== '' && workout.name"> {{workout.name}} Kock</h4>
+    <h4 v-else v-html="workout.date"></h4>
+    <v-card v-if="workout">
+      <SetPanel v-for="set in workout.sets" :key="set.id" :set='set'/>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import SetPanel from '@/components/SetPanel'
+export default {
+   data(){
+    return{
+      
+    }
+   },
+   props: [
+     "workout"
+   ],
+   methods:{
+
+   },
+   components: {
+     SetPanel
+   }
+}
+</script>
+
+<style>
+
+</style>
