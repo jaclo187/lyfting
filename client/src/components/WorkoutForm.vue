70f44b8a53c463fc2d68b94070a7c5a19a1ab617
Louis <jaclo187@school.lu>
2020-03-02 07:59:07 +0100

    Created form to create a workout

---
+++ client/src/components/WorkoutForm.vue
@@ -0,0 +1,21 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    data(){
+        return {}
+    },
+    props : [
+
+    ],
+    methods : [
+
+    ]
+}
+</script>
+
+<style>
+
+</style>
