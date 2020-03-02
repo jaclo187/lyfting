3b531c8c4772e34363937ae20273490c0b4bdc98
Louis <jaclo187@school.lu>
2020-03-02 08:04:28 +0100

    Added workout route to express

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,9 @@
 const Authentication = require('./controllers/Authentication')
 const AuthenticationPolicy = require('./policies/AuthenticationPolicy')
+const Workouts = require('./controllers/Workouts')
 
 module.exports = (app) => {
     app.post('/register', AuthenticationPolicy.register, Authentication.register)
     app.post('/login', Authentication.login)
+    app.post('/workouts', Workouts.index)
 }
