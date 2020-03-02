5f94a40e182a7b7f0f5f988f01a1d28ade0730e0
Louis <jaclo187@school.lu>
2020-03-02 07:56:10 +0100

    Created vuetify panel

---
+++ client/src/components/Panel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="white elevation-2">
+      <v-toolbar text dense class="blue">
+          <v-toolbar-title>{{title}}</v-toolbar-title>
+      </v-toolbar>
+
+      <div class="pl-4 pr-4 pt-2 pb-2">
+          <slot>
+              Nothing here.
+          </slot>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'title'
+  ]
+}
+</script>
+
+<style scoped>
+</style>
