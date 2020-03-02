ced8f440256737e53d34d5c4a7213b711b9db0c3
Louis <jaclo187@school.lu>
2020-03-02 07:58:39 +0100

    Set panel uses now db data

--- client/src/components/SetPanel.vue
+++ client/src/components/SetPanel.vue
@@ -1,28 +1,37 @@
 <template>
   <div>
     <h5>{{set.name}}</h5>
-    <v-flex class=".flex-row">
-        <v-container v-if="set.data.reps">
-            <v-card v-for="(rep, index) in set.data.reps" :key="`rep-${index}`">
-                {{rep}}
+    <v-layout row>
+
+        <v-card v-for="data in set.data" :key='data.id'>
+            <v-card v-for="(item, key) in data" :key="key">
+                <span v-if="key == 'weight' && item !== null">{{item}}kg</span>
+                <span v-if="key == 'reps' && item !== null">{{item}}x</span>
+                <span v-if="key == 'time' && item !== null">{{item}}m</span>
             </v-card>
-        </v-container>
-        <v-container v-if="set.data.weight">
+        </v-card>
+
+        <!-- <v-flex v-if="set.data.reps" class="md3 mt-1">
+            <v-card v-for="(rep, index) in set.data.reps" :key="`rep-${index}-${set.id}`">
+                {{rep}} x
+            </v-card>
+        </v-flex>
+        <v-flex v-if="set.data.weight" class="md3 mt-1">
             <v-card 
                 v-for="(weight, index) in set.data.weight" 
-                :key="`weight-${index}`">
+                :key="`weight-${index}-${set.id}`">
                 {{`${weight}
                    ${$store.state.user.preferenceWeight == 'kilogramm' ? "kg" : "lbs"}`}}
             </v-card>
-        </v-container>
-        <v-container v-if="set.data.time">
+        </v-flex>
+        <v-flex v-if="set.data.time" class="md3 mt-1">
             <v-card 
                 v-for="(time, index) in set.data.time" 
-                :key="`time-${index}`">
-                {{weight}}
+                :key="`time-${index}-${set.id}`">
+                {{time}} minutes
             </v-card>
-        </v-container>
-    </v-flex>
+        </v-flex> -->
+    </v-layout>
   </div>
 </template>
 
@@ -33,7 +42,10 @@ export default {
     },
     props : [ "set" ],
     methods : {
-
+        
+    },
+    mounted(){
+        console.log(this.set)
     }
 }
 </script>
