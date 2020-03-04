d337f5dd7518b5bc2da87ff8ebbdc39485f3243c
Louis <jaclo187@school.lu>
2020-03-04 09:31:10 +0100

    implemented CRUD methods

--- client/src/services/WorkoutService.js
+++ client/src/services/WorkoutService.js
@@ -1,7 +1,17 @@
 import Api from '@/services/Api'
 
 export default {
-  index(token) {
-    return Api().post('workouts', token)
+  index(data) {
+    return Api().post('workouts', data)
+  },
+  post(token) {
+    return Api().post('workouts/create', token)
+  },
+  update(token, id, name) {
+    //name == new name
+    Api().patch('workouts/update', {token, id, name})
+  },
+  delete(token, id) {
+    Api().delete(`workouts/delete/${id}`, { data: { }, headers: { "Authorization": token } })
   }
 }
