959f0ec15f64aae5a1a1555550f8023cd70d3f2f
Louis <jaclo187@school.lu>
2020-03-04 09:32:13 +0100

    Created "editworkout" view

---
+++ client/src/views/EditWorkout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>{{workout}}</div>
+</template>
+
+<script>
+import WorkoutService from '@/services/WorkoutService'
+export default {
+  data () {
+    return {
+      workout : null
+    }
+  },
+  props : [
+
+  ],
+  async mounted(){
+    if(this.workout === null){
+      this.workout = (
+        await WorkoutService.index(
+          {
+            token: this.$store.state.token, 
+            id: this.$route.params.id
+          }
+        )
+      ).data
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
