31d87e0b4be7cf72283ed8cc13a73b2cbeb8f403
Louis <jaclo187@school.lu>
2020-03-04 09:33:35 +0100

    added config support for mysql2

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,12 +1,18 @@
+const bluebird = require('bluebird')
+
 module.exports = {
     port : process.env.PORT || 8080,
     db : {
+        host: 'localhost',
         database: process.env.DB_ENV || "lyfting",
+        port: 3306,
         user: process.env.DB_ENV || "node",
         password: process.env.DB_PASS || "passw0rd",
+        Promise: bluebird,
         options: {
             dialect: process.env.DIALECT || "mysql",
             host: process.env.HOST || "localhost",
+            charset: 'utf8',
             storage: "./lyfting.sql",
             define: {
                 timestamps: false
