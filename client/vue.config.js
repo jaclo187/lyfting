ac7d28988335ebbc0f26fa7a5329999f9a050fcc
Louis <jaclo187@school.lu>
2020-03-09 11:00:20 +0100

    moved public render folder to server folder

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,13 @@
+const path = require('path')
+
 module.exports = {
-  transpileDependencies: ['vuetify']
+  transpileDependencies: ['vuetify'],
+  outputDir: path.resolve(__dirname, '../server/public'),
+  devServer: {
+    proxy: {
+      '/' : {
+        target: 'http://localhost:8080'
+      }
+    }
+  }
 }
