b30e7ca1c98106caea124201f1ad8506a27cbc00
Louis <jaclo187@school.lu>
2020-03-09 11:03:11 +0100

    Implemented https

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,18 @@
+const path = require("path")
+const fs = require("fs")
+require('dotenv').config({ path: path.join(__dirname, '.env') })
 const express = require("express")
 const bodyParser = require('body-parser')
 const cors = require("cors")
 const {sequelize} = require("./models")
 const morgan = require("morgan")
 const config = require('./config/config')
+
+const https = require('https');
+const privateKey = fs.readFileSync(path.resolve("/etc/letsencrypt/live/lyfting.fit/privkey.pem"), 'utf8');
+const certificate = fs.readFileSync(path.resolve("/etc/letsencrypt/live/lyfting.fit/cert.pem"), 'utf8');
+
+const credentials = {key: privateKey, cert: certificate};
 
 const app = express()
 app.use(morgan('combined'))
@@ -12,9 +21,16 @@ app.use(cors())
 
 require('./routes')(app)
 
-app.use(express.static('../public'));
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.resolve(__dirname + '/../public')));
+    app.get(/.*/, (req, res) => res.sendFile(path.resolve(__dirname + '/../public/index.html')))
+}
+
+const httpsServer = https.createServer(credentials, app);
 
 sequelize.sync()
     .then(() => {
-        app.listen(config.port)
+        //app.listen(config.port)
+        httpsServer.listen(config.port)
+        console.log(`Server running on port ${config.port}`)
     })
