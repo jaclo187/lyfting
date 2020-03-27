6346a5ab0492bbb5c582ae461dd6c2e3354b9902
Louis <jaclo187@school.lu>
2020-03-27 09:24:22 +0100

    Updated layout List character of exercise is uppercase now UI shows EMPTY EXERCISE in overview

--- client/src/components/SetPanel.vue
+++ client/src/components/SetPanel.vue
@@ -1,18 +1,23 @@
 <template>
-  <div>
-    <h5>{{set.name}}</h5>
-    <v-layout row>
-        <v-card>Exercise: {{set.exercise}} using {{set.type}}</v-card><br>
-        <v-card>Musclegroup: {{set.musclegroup}}</v-card><br>
-        <v-card v-for="data in set.data" :key='data.id'>
+    <v-layout v-if="set.exercise" row>
+        <v-card class="set_card">
+            Exercise: 
+            <span class="bold">{{transformString(set.exercise)}}</span> 
+            for 
+            <span class="bold">{{transformString(set.musclegroup)}}</span>
+        </v-card>
+        <br>
+        <v-card v-for="data in set.data" :key='data.id' class="set_card">
             <v-card v-for="(item, key) in data" :key="key">
                 <span v-if="key == 'weight' && item !== null">{{item}}kg</span>
                 <span v-if="key == 'reps' && item !== null">{{item}}x</span>
-                <span v-if="key == 'time' && item !== null">{{item}}m</span>
+                <span v-if="key == 'time' && item !== null">{{item}}min</span>
             </v-card>
         </v-card>
     </v-layout>
-  </div>
+    <v-layout v-else row>
+        <v-card class="set_card">&lt;Empty Exercise&gt;</v-card>
+    </v-layout>
 </template>
 
 <script>
@@ -20,10 +25,21 @@ export default {
     data(){
         return{}
     },
-    props : [ "set" ]
+    props : [ "set" ],
+    methods : {
+        transformString(string){
+            return string.charAt(0).toUpperCase() + string.slice(1)
+        }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+    .set_card {
+        padding: 0.2vw;
+        margin: 0.2vw;
+    }
+    .bold {
+        font-weight: bold;
+    }
 </style>
